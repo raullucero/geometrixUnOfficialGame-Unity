86973993f2bcdeb114060cc91f191dff1cf889d6
Raul <raul.lucero27@gmail.com>
2015-01-16 12:20:44 -0600

    [add] Estructura del proyecto

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void changeScene (){
+		Application.LoadLevel ("Scene Geometrix");
+	}	
+}
---
+++ Assets/Scripts/TrianguloScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrianguloScript : MonoBehaviour
+{
+
+		Transform player;
+		float move;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				move = -.3f;
+				player = GameObject.Find ("Cubito").transform;
+
+				rigidbody2D.velocity = new Vector2 (move, move);
+		}
+}
