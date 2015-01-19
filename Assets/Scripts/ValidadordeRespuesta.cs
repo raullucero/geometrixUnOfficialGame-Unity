94af3dc60e129d0e1497674f8dffdbdff63a8ad0
Alejandro Medina <5almero5@gmail.com>
2015-01-19 13:08:16 -0600

    Se coloca scrip a las botones de gui para restablecer el tiempo,

--- Assets/Scripts/ValidadordeRespuesta.cs
+++ Assets/Scripts/ValidadordeRespuesta.cs
@@ -5,7 +5,8 @@ public class ValidadordeRespuesta : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void SelectRespuesta () {
+	public void SelectRespuesta () {
+		// cuando preciona un boton se reestablece el tiempo 
 		Time.timeScale = 1.0f;
 	}
 }
