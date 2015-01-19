392e468c7082d5a98bd2420a0058f4102e7b0c57
Alejandro Medina <5almero5@gmail.com>
2015-01-19 12:34:58 -0600

    Mas plataformas del nivel

---
+++ Assets/Scripts/ValidadordeRespuesta.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValidadordeRespuesta : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void SelectRespuesta () {
+		Time.timeScale = 1.0f;
+	}
+}
